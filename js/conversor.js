4e1c96df4a1c581fc9646c5547a089a8d012d38b
Diogo Falqueto <diogo.m.falqueto@protonmail.com>
2022-05-02 19:57:40 -0300

    Finalização do conversor
    -conversor de numeros octal e hexadecimal adicioando.
    -adição de css.

--- js/conversor.js
+++ js/conversor.js
@@ -1,23 +1,39 @@
 function start(){
-	var inputValue = getInputValue()
-	var resultadoValue = converter(inputValue, 2)
-	responder(resultadoValue)
+	var inputValue = getInput()
+	var base = getBaseChecked()
+	var returnValue = converter(inputValue, base)
+	setOutput(returnValue)
 	}
 
-function getInputValue(){
+function getInput(){
 	var inputElement = document.getElementById('input')
 	var decimalValue = new Number(inputElement.value)
-	console.log('Input Value: '+decimalValue)
+	//console.log('Input Value: '+decimalValue)
 	return decimalValue
 	}
+
+function getBaseChecked(){
+	var binario = document.getElementById("binario")
+	var octal = document.getElementById("octal")
+	var hexadecimal = document.getElementById("hexadecimal")
+	var checked
+	if(binario.checked){
+		checked = binario
+	}else if(octal.checked){
+		checked = octal
+	}else if(hexadecimal.checked){
+		checked = hexadecimal
+	}
+	return checked.value
+}
 
 function converter(value, base){
 	return value.toString(base)
 	}
 
-function responder(returnValue){
-	var ResponseElement = document.getElementById('Resposta')
-	ResponseElement.value = returnValue
+function setOutput(returnValue){
+	var OutputElement = document.getElementById('output')
+	OutputElement.value = returnValue
 	}
 
 
